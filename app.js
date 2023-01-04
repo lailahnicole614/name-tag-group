4332d54fd0fd89a970f9bb0b60933848a77c80e5
ANTHONY WOO <anthony.woo14@gmail.com>
2023-01-04 16:33:49 -0700

    added some javascript

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 /* Imports */
+const nameInput = document.getElementById('name-input');
+const submitBtn = document.getElementById('submit-button');
+const nameDisplay = document.getElementById('name');
+
+console.log('nameInput', nameInput);
+console.log('submitBtn', submitBtn);
+console.log('nameDisplay', nameDisplay);
 
 /* Get DOM Elements */
 
 /* State */
 
 /* Events */
+submitBtn.addEventListener('click', () => {
+    console.log('clicking the button');
+    nameDisplay.textContent = nameInput.value;
+    nameInput.value = ' ';
+});
 
 /* Display Functions */
 
